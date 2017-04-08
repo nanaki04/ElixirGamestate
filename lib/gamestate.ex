d581add10d8a2ba413f8ea8c8c7f51ffdd0c548d
Robert Jan Zwetsloot <zwetsloot.robert.jan@techcross.co.jp>
2017-04-08 12:32:37 +0900

    initialized project to handle generic gamestates

---
+++ lib/gamestate.ex
@@ -0,0 +1,18 @@
+defmodule Gamestate do
+  @moduledoc """
+  Documentation for Gamestate.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gamestate.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gamestate_test.exs
@@ -0,0 +1,8 @@
+defmodule GamestateTest do
+  use ExUnit.Case
+  doctest Gamestate
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
