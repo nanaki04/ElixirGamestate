3293d69b246eb40097a3d572026d7827a17113b7
Robert Jan Zwetsloot <zwetsloot.robert.jan@techcross.co.jp>
2017-04-08 14:30:55 +0900

    added the Gamestate module, and removed the supervision on start

--- lib/gamestate.ex
+++ lib/gamestate.ex
@@ -1,18 +1,35 @@
 defmodule Gamestate do
-  @moduledoc """
-  Documentation for Gamestate.
-  """
+  use Supervisor
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Gamestate.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def start_link do
+    {:ok, supervisor} = Supervisor.start_link(__MODULE__, [])
+    {:ok, agent} = Agent.start_link(fn -> %{} end)
+    {supervisor, agent}
   end
+
+  def init([]) do
+    children = [
+      worker(Gamestate, [], restart: :transient)
+    ]
+    supervise(children, strategy: :simple_one_for_one)
+  end
+
+  def add_substate({supervisor, agent}, name) do
+    {:ok, substate} = Supervisor.start_child(supervisor, [])
+    Agent.update(agent, &Map.put(&1, name, substate))
+    substate
+  end
+
+  def get_substate({_supervisor, agent}, name) do
+    Agent.get(agent, &Map.get(&1, name))
+  end
+
+  def update_value({_supervisor, agent}, name, value) do
+    Agent.update(agent, &Map.put(&1, name, value))
+  end
+
+  def get_value({_supervisor, agent}, name) do
+    Agent.get(agent, &Map.get(&1, name))
+  end
+
 end
